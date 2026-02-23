083596407a420c3d82e73a31a0fa5fcb94ab4fbe
Julio Bezerra de Mattos <julio.mattos@byseven.com.br>
2026-02-23 13:32:20 -0300

    Implementado métodos Http nas controllers User e Category.

--- Project_FinanceControl/Controllers/CategoryController.cs
+++ Project_FinanceControl/Controllers/CategoryController.cs
@@ -1,7 +1,6 @@
 ﻿using FinanceCotrol.Context;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using FinanceCotrol.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Project_FinanceControl.Controllers
@@ -21,20 +20,20 @@ namespace Project_FinanceControl.Controllers
         public ActionResult<IEnumerable<Category>> Get()
         {
             var categories = _context.Categories.Include(c => c.User).ToList();
-            if(categories == null)
+            if (categories == null)
             {
                 return BadRequest("Categorias não encontradas...");
             }
             return Ok(categories);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "ObterCategoria")]
         public ActionResult<Category> Get(int id)
         {
             var category = _context.Categories
                 .Include(c => c.User)
                 .FirstOrDefault(c => c.CategoryId == id);
-            if(category == null)
+            if (category == null)
             {
                 return NotFound("Categoria não encontrada...");
             }
@@ -44,13 +43,39 @@ namespace Project_FinanceControl.Controllers
         [HttpPost]
         public ActionResult Post(Category category)
         {
-            if(category == null)
+            if (category == null)
             {
-                return BadRequest("Categoria invalida...");
+                return BadRequest();
             }
             _context.Categories.Add(category);
             _context.SaveChanges();
-            return Ok("Categoria criada com sucesso...");
+            return new CreatedAtRouteResult("ObterCategoria",
+                 new { id = category.CategoryId }, category);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return BadRequest("Id invalido...");
+            }
+            _context.Entry(category).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound("Categoria não encontrada...");
+            }
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return Ok(category);
         }
     }
 }
--- Project_FinanceControl/Controllers/UserController.cs
+++ Project_FinanceControl/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using FinanceCotrol.Context;
 using FinanceCotrol.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceCotrol.Controllers;
 
@@ -26,11 +27,11 @@ public class UserController : ControllerBase
         return Ok(users);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "ObterUsuario")]
     public ActionResult<User> Get(int id)
     {
-        var user = _context.Users.FirstOrDefault(c=> c.UserId == id);
-        if(user == null)
+        var user = _context.Users.FirstOrDefault(c => c.UserId == id);
+        if (user == null)
         {
             return NotFound("Usuario não encontrado...");
         }
@@ -40,19 +41,33 @@ public class UserController : ControllerBase
     [HttpPost]
     public ActionResult Post(User user)
     {
-        if(user == null)
+        if (user == null)
         {
-            return BadRequest("Usuario invalido");
+            return BadRequest();
         }
         _context.Users.Add(user);
-        return Ok("Usuario criado com sucesso");
+        _context.SaveChanges();
+        return new CreatedAtRouteResult("ObterUsuario",
+            new { id = user.UserId }, user);
+    }
+
+    [HttpPut("{id:int}/user-update")]
+    public ActionResult Put(int id, User user)
+    {
+        if (id != user.UserId)
+        {
+            return BadRequest("Id invalido...");
+        }
+        _context.Entry(user).State = EntityState.Modified;
+        _context.SaveChanges();
+        return Ok(user);
     }
 
     [HttpDelete("{id}")]
     public ActionResult Delete(int id)
     {
         var user = _context.Users.Find(id);
-        if(user == null)
+        if (user == null)
         {
             return NotFound("Id invalido");
         }
@@ -60,5 +75,4 @@ public class UserController : ControllerBase
         _context.SaveChanges();
         return Ok("Usuario deletado com sucesso");
     }
-
 }
