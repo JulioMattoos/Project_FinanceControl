b8e70581f6346d10d869abd45f5dce42c65ad2c9
Julio Bezerra de Mattos <julio.mattos@byseven.com.br>
2026-03-03 13:07:02 -0300

    Criado TransactioRepository

---
+++ Project_FinanceControl/Repository/Transaction/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using FinanceCotrol.Models;
+
+namespace Project_FinanceControl.Repository;
+
+public interface ITransactionRepository
+{
+    IEnumerable<Transaction> GetAllTransactions();
+
+    Transaction GetTransactionById(int id);
+
+    Transaction CreateTransaction(Transaction transaction);
+
+    Transaction UpdateTransaction(Transaction transaction);
+
+    Transaction DeleteTransaction(int id);
+}
